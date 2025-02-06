529f5862184034afde87aea6b4c60198288a3850
EA - Gabriel Santos Sales <santos_sales@educadventista.org>
2025-02-06 15:49:26 -0300

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+      <div>
+       <h1>Hellow wold</h1>
+       <p>ababbababbababbaabbabbabbbababababbababbababababbabababbababababBANANA</p>
+      </div>
+      
+  )
+}
+
+export default App
